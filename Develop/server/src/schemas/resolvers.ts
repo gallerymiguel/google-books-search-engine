32fe1166faaab3344805e6709f38bdd10f767cef
Miguel <galleriesproductions@gmail.com>
2024-12-20 19:06:03 -0500

    more rough draft schemas

--- Develop/server/src/schemas/resolvers.ts
+++ Develop/server/src/schemas/resolvers.ts
@@ -1,6 +1,99 @@
-export const resolvers = {
-    Query: {
-      hello: () => 'Hello world!',
-    },
+import { User } from '../models/index.js';
+import { signToken, AuthenticationError } from '../utils/auth.js'; 
+
+// Define types for the arguments
+interface AddUserArgs {
+  input: {
+    username: string;
+    email: string;
+    password: string;
   };
-  
+}
+
+interface LoginArgs {
+  email: string;
+  password: string;
+}
+
+interface SaveBookArgs {
+  input: {
+    bookId: string;
+    authors: string[];
+    description: string;
+    title: string;
+    image: string;
+    link: string;
+  };
+}
+
+interface RemoveBookArgs {
+  bookId: string;
+}
+
+const resolvers = {
+  Query: {
+    // Get the authenticated user's data
+    me: async (_parent: any, _args: any, context: any) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('Could not authenticate user.');
+    },
+  },
+  Mutation: {
+    // Register a new user
+    addUser: async (_parent: any, { input }: AddUserArgs) => {
+      const user = await User.create({ ...input });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Log in an existing user
+    login: async (_parent: any, { email, password }: LoginArgs) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Could not authenticate user.');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Could not authenticate user.');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Save a book to the user's profile
+    saveBook: async (_parent: any, { input }: SaveBookArgs, context: any) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } }, // Prevent duplicate entries
+          { new: true, runValidators: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    // Remove a book from the user's profile
+    removeBook: async (_parent: any, { bookId }: RemoveBookArgs, context: any) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } }, // Remove the specific book
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+export default resolvers;
--- Develop/server/src/schemas/typeDefs.ts
+++ Develop/server/src/schemas/typeDefs.ts
@@ -1,7 +1,55 @@
-import { gql } from 'apollo-server-express';
+import { gql } from '@apollo/server';
 
 export const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  input BookInput {
+    bookId: String!
+    authors: [String]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
   type Query {
-    hello: String
+    me: User
+    users: [User]
+    user(username: String!): User
+    books: [Book]
+    book(bookId: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(input: UserInput!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
